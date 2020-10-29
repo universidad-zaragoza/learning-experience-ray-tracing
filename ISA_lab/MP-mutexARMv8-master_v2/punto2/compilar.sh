f2692346322f1989faa746e869c35cd0fe6aeb61
RubÃen Gran Tejero <rgran@unizar.es>
2020-10-29 17:22:50 +0100

    update folder hierarchy

--- ISA_lab/MP-mutexARMv8-master_v2/punto2/compilar.sh
+++
@@ -1,2 +0,0 @@
-gcc mi_fa.s -c
-g++ ej2.cpp mi_fa.o -o ej2 -lpthread
--- ISA_lab/MP-mutexARMv8-master_v2/punto3/compilar.sh
+++
@@ -1,5 +0,0 @@
-gcc mi_fa.s -c
-gcc mi_mutex.s -c 
-gcc mi_mutex_sleep.s -c 
-g++ ej3.cpp mi_fa.o mi_mutex.o -o ej3 -lpthread
-g++ ej3.cpp mi_fa.o mi_mutex_sleep.o -o ej3_sleep -lpthread
